5298e182804b9dfd4db8624187762efd9654f05d
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-01 18:06:53 -0800

    guard statements

--- AdventureSwift_Starter_Working/Ch4_Control_Flow.playground/Pages/Using the Guard Statement.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter_Working/Ch4_Control_Flow.playground/Pages/Using the Guard Statement.xcplaygroundpage/Contents.swift
@@ -23,6 +23,13 @@ let currentGold = 16
 
 // Guard statement with for-in loop
 
+for (item, price) in shopItems {
+    guard currentGold >= price else {
+        print("You can't afford the \(item)")
+        continue
+    }
+    print ("Go ahead, the \(item) is yours for \(price) gold!")
+}
 
 // Guard check for specific item
 
