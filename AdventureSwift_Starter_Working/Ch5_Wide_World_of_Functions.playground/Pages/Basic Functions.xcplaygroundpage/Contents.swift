cc3f9ad92557c2a6efcbc7c970622ca879190fbd
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-04 22:51:26 -0800

    basic function syntax

--- AdventureSwift_Starter_Working/Ch5_Wide_World_of_Functions.playground/Pages/Basic Functions.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Starter_Working/Ch5_Wide_World_of_Functions.playground/Pages/Basic Functions.xcplaygroundpage/Contents.swift
@@ -15,3 +15,11 @@
  */
 // Basic function
 
+func findNearestAlly(level:Int) -> String {
+    print("Searching for ally above level \(level)")
+    return "Argus"
+}
+
+var ally = findNearestAlly(level: 5)
+
+
